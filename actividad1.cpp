2e95ecd044fb1d321fbaf9f4d756afa2f6b58023
paduvireh <paduvireh@unjbg.edu.pe>
2024-06-29 21:57:33 -0500

    Añadiendo actividad1

---
+++ actividad1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float r, A;
+	cout<<"Introduce el valor del radio: ";
+	cin>>r;
+	
+	A=3.14*r*r;
+	cout<<"El area del circulo es: "<<A<<endl;
+	
+	return 0;
+}
