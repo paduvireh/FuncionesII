61c18a14f2dff8cc0cb3155fb0cb8da2b5f535a2
paduvireh <paduvireh@unjbg.edu.pe>
2024-06-30 01:34:07 -0500

    Actividad3: Se agrego la funcion Hipotenusa

--- actividad3.cpp
+++ actividad3.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+float Hipotenusa(float cat1, float cat2){
+	return sqrt(pow(cat1, 2)+pow(cat2, 2));
+}
+
 int main(){
 	float c1, c2, h;
 	cout<<"Ingrese el primer cateto: ";
@@ -10,7 +14,7 @@ int main(){
 	cout<<"Ingrese el segundo cateto: ";
 	cin>>c2;
 	
-	h= sqrt(pow(c1, 2)+pow(c2, 2));
+	h= Hipotenusa(c1, c2);
 	
 	cout<<"El valor de la hipotenusa es: "<<h<<endl;
 	return 0;
