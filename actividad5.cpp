0485bfc5ddcf2c168d0e8d7720380325d16904ff
paduvireh <paduvireh@unjbg.edu.pe>
2024-06-30 23:15:26 -0500

    Actividad5: Se agrego calculo de las raices complejas

--- actividad5.cpp
+++ actividad5.cpp
@@ -7,16 +7,19 @@ void Raices(float a, float b, float c, float &x1, float &x2, bool &reales) {
     
     if(d>=0){
         if(d==0){
-            x1=((-1*b)+sqrt(d))/(2*a);
+            x1=((-b)+sqrt(d))/(2*a);
             x2=x1;
             reales=true;
         } else {
-	        x1=((-1*b)+sqrt(d))/(2*a);
-	        x2=((-1*b)-sqrt(d))/(2*a);
+	        x1=((-b)+sqrt(d))/(2*a);
+	        x2=((-b)-sqrt(d))/(2*a);
             reales=true; 
         }
-    } else 
+    } else {
+    	x1=-b/(2*a);
+    	x2=sqrt(-d)/(2*a);
         reales=false; 
+    }
 }
 
 int main(){
@@ -35,7 +38,7 @@ int main(){
         else 
             cout<<"Las raices son: "<<x1<<" y "<<x2<<endl;
     } else 
-        cout<<"Las raices son complejas."<<endl;
+        cout<<"Las raices complejas son: "<<x1<<"+"<<x2<<"i"<<" y "<<x1<<"-"<<x2<<"i"<<endl;
     
     return 0;
 }
